b00bfa6aafa5fa93fa601614089ee68c0e35b063
2094687980@qq.com <2094687980@qq.com>
2019-11-24 22:54:24 +0800

    1

--- Zero.Core.App.WebApi/Controllers/ValuesController.cs
+++ Zero.Core.App.WebApi/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace Zero.Core.App.WebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2" };   
         }
 
         // GET api/values/5
