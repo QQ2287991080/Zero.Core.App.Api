bfd62dea837ae6a6a34784c1efc469794eff647f
张力 <2287991080@qq.com>
2019-11-24 22:04:24 +0800

    添加项目文件。

---
+++ Zero.Core.App.WebApi/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using Zero.Core.App.WebApi.Interceptor;
+
+namespace Zero.Core.App.WebApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+
+            //配置Swagger
+            services.AddSwaggerGen(i =>
+            {
+                i.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Zero的应用 WebApi接口文档",
+                    Description = "WebApi",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Zero", Email = "2287991080@qq.com", Url = "http://www.baidu.com" }
+                });
+                i.OperationFilter<SwaggerFileUploadFilter>();
+
+                //Set the comments path for the swagger json and ui.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Zero.Core.App.WebApi.xml");
+                i.IncludeXmlComments(xmlPath);//文档中文提示
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(i =>
+            {
+                i.SwaggerEndpoint("/swagger/v1/swagger.json", "Zero API V1");
+                i.ShowExtensions();
+            });
+        }
+    }
+}
